4bc605dc5e45482ff772a28efa3fddb25528c693
Rechn0 <840712443@qq.com>
2022-02-19 13:54:26 +0800

    0219trials

--- themes/yilia-plus/scripts/filters/meta_generator.js
+++ themes/yilia-plus/scripts/filters/meta_generator.js
@@ -1,19 +1,15 @@
 'use strict';
-const config = require('../../package.json');
+
 const defaultConfig = require('../default_config');
-// 自定义控制台打印函数
-const yilia_plus_log = require('../yilia_plus_log');
 
 function hexoMetaGeneratorInject(data) {
-  /* /i表示不区分大小写 */
-  if (!defaultConfig.meta_generator || !data ||
+  const config = defaultConfig;
+  if (!config.meta_generator || !data ||
     data.match(/<meta\s+name=['|"]?generator['|"]?/i)) {
-    return data;
+    return;
   }
-  /* if (data.match(/<meta\s+name=['|"]?description['|"]?/i)) {
-    yilia_plus_log("匹配meta `description`成功");
-  } */
-  const hexoGeneratorTag = `\n  <meta name="generator" content="` + config.name + `">`;
+
+  const hexoGeneratorTag = `\n  <meta name="generator" content="hexo-theme-yilia-plus">`;
 
   return data.replace('</title>', '</title>' + hexoGeneratorTag);
 }
